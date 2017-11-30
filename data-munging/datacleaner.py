2056361461aef2f225b969d65562b88432307e7d
Joby Joy <jj2196@nyu.edu>
2017-11-30 13:06:07 -0500

    Parsing date object to integer

--- data-munging/datacleaner.py
+++ data-munging/datacleaner.py
@@ -9,16 +9,15 @@ def checkFromDate(x):
     if len(date) != 3 :
         return 'NULL'
     try:
-        m=date[0]
-        d=date[1]
-        y=date[2]
+        m=int(date[0])
+        d=int(date[1])
+        y=int(date[2])
     except Exception as e:
         return 'NULL'
     if m not in range(1,13) or d not in range(1,32) or y not in range(2010,2017):
-        return 'Invalid'
+        return 'INVALID'
     else:
-        return 'Valid'
-
+        return 'VALID'
 
 
 sc = SparkContext();
@@ -28,9 +27,10 @@ csvData = sc .textFile("NYPD_Complaint_Data_Historic.csv");
 crimeData = csvData.mapPartitions(lambda x:reader(x));
 
 #Data Cleaning for valid time stamp: from_date
+#Code snippet just takes the main RDD and creates a new RDD with the Valid date checker return output
 fromDate = crimeData.map(lambda x: (x[0], (x[1], "DATE", "Complaint from date","date",checkFromDate(x[1]))))
 fromDate.take(5);
-#Save to filte
+#Save to file
 fromDate.map(lambda x:x[1]).saveAsTextFile("fromdate.out")
 
 
